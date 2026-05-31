4024050d551f60f8db62cb1931394b829f2c766a
islam-alim <islamalimzhan0@gmail.com>
2026-05-31 15:02:13 +0500

    downloading via osmnx, shortest path between nearest nodes to the stations

--- project.py
+++ project.py
@@ -4,19 +4,52 @@ import matplotlib.pyplot as plt
 
 place_name = "Almaty, Kazakhstan"
 
-# get the road map
+# road graph
 G = ox.graph_from_place(place_name, network_type="drive")
 
-#G = ox.project_graph(G)
+# bus stops
+stops = ox.features_from_place(
+    place_name,
+    tags={"highway": "bus_stop"}
+)
 
-# get the bus_stop edges
-stops = ox.features_from_place(place_name, tags={"highway": "bus_stop"})
+# make sure CRS matches graph CRS
+stops = stops.to_crs(ox.graph_to_gdfs(G, nodes=False).crs)
+
+# store nearest graph node for each stop
+nearest_nodes = []
+
+for idx, row in stops.iterrows():
+
+    # point geometry
+    point = row.geometry
+
+    lon = point.x
+    lat = point.y
+
+    # nearest node in road graph
+    nearest = ox.distance.nearest_nodes(G, X=lon, Y=lat)
+
+    nearest_nodes.append(nearest)
+
+# add node ids into dataframe
+stops["graph_node"] = nearest_nodes
+
+print(stops[["name", "graph_node"]].head())
 
 
-# plot the graph
+source = stops.iloc[0]["graph_node"]
+target = stops.iloc[5]["graph_node"]
+
+route = nx.shortest_path(
+    G,
+    source=source,
+    target=target,
+    weight="length"
+)
+
+print(route)
+
 fig, ax = ox.plot_graph(G, show=False, close=False)
-
-#plot the bus stops
-stops.plot(ax=ax, color="red", markersize=10)
-
+stops.plot(ax=ax, color="red", markersize=3)
 plt.show()
